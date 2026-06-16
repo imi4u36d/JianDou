d0d263e4c6c7299201f0feff43f6d1362e331f86
imi4u36d <wxwzmail@gmail.com>
2026-06-16 14:36:22 +0800

    chore: remove old packages replaced by renamed api, domain, ui packages

--- packages/frontend-ui/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProgressBar, createProgressBarViewModel } from "./components/ProgressBar";
-export { STATUS_BADGE_TONES, StatusBadge, createStatusBadgeViewModel } from "./components/StatusBadge";
-export type { ProgressBarProps, ProgressBarViewModel } from "./components/ProgressBar";
-export type { StatusBadgeProps, StatusBadgeTone, StatusBadgeViewModel } from "./components/StatusBadge";
